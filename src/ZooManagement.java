6948b7dafa09844e9d2075ee968836244a6510c2
faresbrayek2 <faresbrayek2@gmail.com>
2023-10-05 10:44:27 +0100

    updates2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class ZooManagement {
     public static void main(String[] args) {
-/*
+        /*
         Instruction 5 :
         Animal lion = new Animal("Félin", "lion", 3, true);
         Zoo myZoo = new Zoo("Mon Zoo", "Ma Ville", 25);
-*/
+        */
 
 
 
 
-/*      Instruction 6 :
+        /*
+        Instruction 6 :
         On remarque qu'il faut additionner les valeurs des objets car nous avons créé un constructeur
-*/
+        */
 
 
 
@@ -25,12 +26,12 @@ public class ZooManagement {
 
 
 
-//        Instruction 8 :
-
+//      Instruction 8 :
         Zoo myZoo = new Zoo("Mon Zoo", "Ma Ville");
         myZoo.displayZoo();
         System.out.println(myZoo);
         System.out.println(myZoo.toString());
+
         /*
         output de instruction  myZoo.displayZoo();
         Nom du zoo : Mon Zoo
@@ -41,17 +42,18 @@ public class ZooManagement {
         Zoo@5f184fc6
         Zoo@5f184fc6
         */
+
         /*
         Lorsque vous utilisez System.out.println(myZoo); et System.out.println(myZoo.toString());
         et que la classe Zoo n'a pas redéfini la méthode toString()
         donc ils afficher adresse mémoire
         */
 
-//        Instruction 9 :
-//        pour corriger cette erreur on doit ajouter methode toString()
+//      Instruction 9 :
+//      pour corriger cette erreur on doit ajouter methode toString()
 
 
-//        Instruction 10 :
+//      Instruction 10 :
         // on propos number de cages est 2
         Animal tigre = new Animal("Félin", "Tigre", 8, true);
         boolean animal1 = myZoo.addAnimal(lion);
@@ -72,15 +74,15 @@ public class ZooManagement {
         } else {
             System.out.println("L'animal 3 n'a pas pu être ajouté au zoo.");
         }
+
         /*
         output:
         L'animal 1 a été ajouté au zoo avec succès !
         L'animal 2 a été ajouté au zoo avec succès !
         L'animal 3 n'a pas pu être ajouté au zoo.
 
-
         On note le nombre maximum d'animaux peuvent être ajoutés inferieur la valeur de nbrcages
-         dans ce cas nombre de cages est 2 on peut ajouter 2 animaux .
+        dans ce cas nombre de cages est 2 on peut ajouter 2 animaux .
 
          */
 
@@ -88,6 +90,7 @@ public class ZooManagement {
 //      Instruction 11 :
         System.out.println("------------------------ Instruction 11 -------------------------");
         myZoo.displayAnimals();
+
         /*
         output :
         Animal 1:
@@ -95,6 +98,7 @@ public class ZooManagement {
         Animal 2:
         Animal{family='Félin', name='Tigre', age=8, isMammal=true}
          */
+
         Animal animalRecherche1 = new Animal("Félin", "lion", 3, true);
         int index1 = myZoo.searchAnimal(animalRecherche1);
         if (index1 != -1) {
@@ -109,6 +113,7 @@ public class ZooManagement {
         } else {
             System.out.println("L'animal " + animalRecherche2.name + " n'a pas été trouvé dans le zoo.");
         }
+
         /*
         output:
         L'animal lion a été trouvé à l'indice: 0
@@ -132,6 +137,7 @@ public class ZooManagement {
             System.out.println("L'animal " + animalRecherche1.name + " n'a pas été trouvé dans le zoo.");
         }
         myZoo.displayAnimals();
+
         /*
         output:
         L'animal lion a été trouvé à l'indice: 0
@@ -148,10 +154,9 @@ public class ZooManagement {
         Animal 4:
         Animal{family='Félin', name='lion', age=3, isMammal=true}
         J'ai remarqué que la méthode renverra l'index du premier animal correspondant trouvé dans le zoo.
-
         */
 
-//       Instruction 12 :
+//      Instruction 12 :
         System.out.println("------------------------ Instruction 12 -------------------------");
         /*
         pour corriger cette erreur on ajouter function boolean pour test si animail existe ou non
@@ -160,7 +165,7 @@ public class ZooManagement {
         - function isAnimalInZoo(animal) est ajouter dans le class Zoo et appelle dans le function addAnimal(animal)
          */
         myZoo.displayAnimals();
-    /*
+        /*
         output :
         L'animal lion a été trouvé à l'indice: 0
         L'animal cat n'a pas été trouvé dans le zoo.
@@ -173,12 +178,12 @@ public class ZooManagement {
         Animal{family='Félin', name='Tigre', age=8, isMammal=true}
         Animal 3:
         Animal{family='Éléphant', name='elephant', age=5, isMammal=true}
-     */
+         */
 
 
 
 
-//        Instruction 13 :
+//      Instruction 13 :
         System.out.println("------------------------ Instruction 13 -------------------------");
         myZoo.displayAnimals();
         Animal lionToRemove = new Animal("Félin", "lion", 3, true);
@@ -190,50 +195,52 @@ public class ZooManagement {
         }
         myZoo.displayAnimals();
         /*
-            output:
-            avant remove:
-            Animaux dans le zoo:
-            Animal 1:
-            Animal{family='Félin', name='lion', age=3, isMammal=true}
-
-            Animal 2:
-            Animal{family='Félin', name='Tigre', age=8, isMammal=true}
-
-            Animal 3:
-            Animal{family='Éléphant', name='elephant', age=5, isMammal=true}
-            *******************
-            L'animal a été supprimé avec succès du zoo.
-            *******************
-            apres remove :
-            Animaux dans le zoo:
-            Animal 1:
-            Animal{family='Félin', name='Tigre', age=8, isMammal=true}
-
-            Animal 2:
-            Animal{family='Éléphant', name='elephant', age=5, isMammal=true}
+        output:
+        avant remove:
+        Animaux dans le zoo:
+        Animal 1:
+        Animal{family='Félin', name='lion', age=3, isMammal=true}
+        Animal 2:
+        Animal{family='Félin', name='Tigre', age=8, isMammal=true}
+        Animal 3:
+        Animal{family='Éléphant', name='elephant', age=5, isMammal=true}
+        *******************
+        L'animal a été supprimé avec succès du zoo.
+        *******************
+        apres remove :
+        Animaux dans le zoo:
+        Animal 1:
+        Animal{family='Félin', name='Tigre', age=8, isMammal=true}
+        Animal 2:
+        Animal{family='Éléphant', name='elephant', age=5, isMammal=true}
          */
 
 
-//         Instruction 14 :
+//      Instruction 14 :
+
         /*
         final private int nbrCages = 25;
          */
-//        Instruction 15 :
+
+//      Instruction 15 :
+        System.out.println("------------------------ Instruction 15 -------------------------");
+
         boolean isZooFull = myZoo.isZooFull();
         if (isZooFull) {
             System.out.println("The zoo is full");
         } else {
             System.out.println("The zoo is not yet full");
         }
-
         /*
         Output:
         The zoo is not yet full.
          */
-//        Instruction 16 :
 
+
+
+//      Instruction 16 :
+        System.out.println("------------------------ Instruction 15 -------------------------");
         Zoo secondZoo = new Zoo("Mon Zoo", "Ma Ville");
-
         Animal lionTosecondZoo = new Animal("Félin", "lion", 8, true);
         secondZoo.addAnimal(lionTosecondZoo);
         Animal tigerTosecondZoo = new Animal("Félin", "tiger", 6, true);
@@ -251,8 +258,6 @@ public class ZooManagement {
         Animal :Animal{family='Félin', name='tiger', age=6, isMammal=true}
         Animal :Animal{family='Éléphant', name='elephant', age=11, isMammal=true}
         Animal :Animal{family='Félin', name='cat', age=2, isMammal=true}
-
-
-         */
+        */
     }
 }
